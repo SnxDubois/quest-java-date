b4c7f690c85a287badf29a084721cf82d499c7ad
Sébastien <snx.dubois@gmail.com>
2019-05-20 17:05:25 +0200

    date

--- TestDate.java
+++ TestDate.java
@@ -2,7 +2,8 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Date;
-import java.util.Scanner; 
+import java.util.Scanner;
+import java.text.ParseException;
 
 class TestDate {
 
@@ -18,6 +19,28 @@ class TestDate {
         sc.close();
 
         // TODO : your code after this line
-        
+        // SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        // Date current = new Date();
+        // System.out.println(sdf.format(current));
+
+        // SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        // try {
+        // Date birthday = format.parse("14/06/1983");
+        // System.out.println(birthday); // Thu Jan 01 00:00:00 CET 1970
+        // } catch (ParseException e) {
+        // e.printStackTrace();
+        // }
+
+        // Calendar c = new GregorianCalendar(2019, 3, 22, 13, 30, 00);
+        // Date d = c.getTime();
+        // System.out.println(sdf.format(d)); // 22/04/2019 13:30:00
+
+        SimpleDateFormat sdfQuest = new SimpleDateFormat("MM/dd/yyyy");
+        Calendar c = new GregorianCalendar(year, month - 1, day);
+        Date d = c.getTime();
+        int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
+        System.out.println("The date is : " + sdfQuest.format(d));
+        System.out.println("The day of the week is : " + dayOfWeek);
+
     }
 }
